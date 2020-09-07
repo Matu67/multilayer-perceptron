73978f0b11eb0b7394e57bcbf53a14214c169e43
Matu67 <matteotempo67@gmail.com>
2020-09-07 18:04:08 -0400

    Further restructuring and implemented node filling

--- framework.py
+++ framework.py
@@ -13,34 +13,22 @@ def activation_func(z):
     return reLU(z)
 
 class Layer():
-    def calc_a(self, l, prev_l):
-        self.a = np.matmul(
-
     def __init__(self, nodes, biases, weights):
         self.a = np.array(nodes)
         self.b = np.array(biases)
-        self.w = np.array(weights)
+        self.W = np.array(weights)
         self.n = len(nodes)
 
 class Network():
     def fill_nodes(self):
-        for i, l in enumerate(self.hiddens):
-            if i is not 0:
-                
-
+        for i in range(1, len(self.layers) - 1):
+            self.layers[i].a = activation_func(np.add(np.matmul(self.layers[i - 1].W, self.layers[i - 1].a), self.layers[i - 1].b))
+    
     # We assume that structure has at least 3 elements
     def __init__(self, x, hiddens_struct, y):
-        self.x = x
-        self.input = Layer(x, np.random.rand(hiddens_struct[0]), 
-                              np.random.rand((len(x), hiddens_struct[0])))
-        self.y = y
-        self.output = Layer(np.zeros(y.shape), [], [])
-        self.hiddens = foldr(lambda a, b: b.insert(0, 
-                                                  Layer(np.zeros(y.shape), 
-                                                        np.random.rand(b[0].n), 
-                                                        np.random.rand((a.n, b[0].n)))), 
-                            [self.output], hiddens_struct)
-        
-    
-
-
+        self.layers = [Layer(x, np.random.rand(hiddens_struct[0]), np.random.rand((len(x), hiddens_struct[0])))]
+        self.layers += foldr(lambda a, b: b.insert(0,Layer(np.zeros(y.shape),
+                                                           np.random.rand(b[0].n),
+                                                           np.random.rand((a.n, b[0].n)))),
+                             [self.output], hiddens_struct)
+        self.output += [Layer(np.zeros(y.shape), [], [])]
