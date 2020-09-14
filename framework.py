771b2c8e8862154319d6ba95d2ab530f3884e351
Matu67 <matteotempo67@gmail.com>
2020-09-14 02:04:13 -0400

    Successfully completed the backpropogation

--- framework.py
+++ framework.py
@@ -12,8 +12,7 @@ class Layer():
         self.a = nodes
         self.b = biases
         self.W = weights
-        self.z = np.array([])
-        self.gradient_C = np.array([])
+        self.z = np.zeros(nodes.shape)
         self.n = len(nodes)
 
 class Network():
@@ -34,11 +33,11 @@ class Network():
             self.layers[i].z = np.add(np.matmul(self.layers[i - 1].W, self.layers[i - 1].a), self.layers[i - 1].b)
             self.layers[i].a = activation_func(self.layers[i].z)
     
-    def calc_der_C_wrt_a(self):
-        der_C_wrt_a = [2 * (self.layers[-1].a - self.y_expected)]
-        for i in reversed(range(len(self.layers) - 1)):
+    def calc_der_C_wrt_a(self, y_expected):
+        der_C_wrt_a = [2 * (self.layers[-1].a - y_expected)]
+        for i in reversed(range(0, len(self.layers) - 1)):
             temp = np.array([])
-            for k in range(0, len(self.layers[i].a)):
+            for k in range(self.layers[i].n):
                 weights = self.layers[i].W[:, k]
                 deriv_z = activation_func(self.layers[i + 1].z, deriv=True)
                 deriv_c = der_C_wrt_a[0]
@@ -46,9 +45,20 @@ class Network():
             der_C_wrt_a.insert(0, temp)
         return der_C_wrt_a
 
+    def back_prop(self, y_expected):
+        der_C_wrt_a = self.calc_der_C_wrt_a(y_expected)
+        gradient_C = []
+        for i in range(0, len(self.layers) - 1):
+            temp = np.matmul(np.resize(self.layers[i + 1].z, (self.layers[i + 1].n, 1)), np.resize(self.layers[i].a, (1, self.layers[i].n)))
+            gradient_C.append(temp * der_C_wrt_a[i])
+            gradient_C.append(temp)
+        return gradient_C
+    
+    def input(self, x):
+        self.layers[0].a = x
+        self.layers[0].z = np.zeros(self.layers[0].n)
+
     def __init__(self, x, hiddens_struct, y):
-        output = [Layer(np.zeros(len(y)), np.array([]), np.array([]))]
+        output = [Layer(np.zeros(y), np.array([]), np.array([]))]
         self.layers = [Layer(x, np.random.rand(hiddens_struct[0]) * 20 - 10, np.random.rand(hiddens_struct[0], len(x)))] + \
                       foldr(lambda a, b: [Layer(np.zeros(a), np.random.rand(b[0].n) * 20 - 10, np.random.rand(b[0].n, a))] + b, output, hiddens_struct)
-        self.y_expected = y
-
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from framework import Network
 import numpy as np
 
-test = Network(np.random.rand(8), [9, 16], [0, 0, 0, 1, 0])
+test = Network(np.random.rand(8), [9, 16], 5)
 test.forward_prop()
 print("--------------------NODES--------------------")
 test.print_nodes()
@@ -9,4 +9,5 @@ print("-------------------WEIGHTS-------------------")
 test.print_weights()
 print("--------------------BIASES-------------------")
 test.print_biases()
-test.calc_der_C_wrt_a()
+test.calc_der_C_wrt_a([0, 0, 0, 1, 0])
+print(test.back_prop([0, 0, 0, 1, 0]))
